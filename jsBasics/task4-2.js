f27f36c86b4ab194e15ba059a27efce98b2adc89
IvannaDb <ivanna.guba@gmail.com>
2024-01-27 13:57:13 +0200

    Added file with 4.2 task

---
+++ jsBasics/task4-2.js
@@ -0,0 +1,6 @@
+let length = (Math.random()*20).toFixed(2)
+console.log ('Random length of rectangle is ', length)
+let width = (Math.random()*20).toFixed(2)
+console.log ('Random width of rectangle is ', width)
+let rectangleArea = length*width
+console.log ('The calculated area of the rectangle is ', rectangleArea.toFixed(2))
