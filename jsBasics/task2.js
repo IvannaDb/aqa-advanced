d83acff545dcdb1d1056607d59a19166e0c8d545
IvannaDb <ivanna.guba@gmail.com>
2024-01-27 13:09:26 +0200

    Added file with 2nd task

---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+const personName1 = 'Alice'
+const personName2 = 'John'
+console.log ('hello'+' '+personName1+' & '+personName2) // конкатинація
+
+
+const greeting = `hello ${personName1} & ${personName2}!` // шаблонний рядок 
+console.log (greeting)
