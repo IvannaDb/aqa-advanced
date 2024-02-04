7adcb1574b3b49201852b49181a1e64301603d0e
IvannaDb <ivanna.guba@gmail.com>
2024-02-04 16:44:13 +0200

    Added changes after review

--- introToFunctions/task2.js
+++ introToFunctions/task2.js
@@ -6,9 +6,16 @@
 // }
 // isUserAdult(15, 25);
 
+// function isUserAdult(age) {
+//         const isAdult=age>= 18
+//         console.log(`Is this person ${age} years old an adult? ${isAdult}`)
+//     }
+// isUserAdult(15)
+// isUserAdult(25)
+
 function isUserAdult(age) {
-        const isAdult=age>= 18
-        console.log(`Is this person ${age} years old an adult? ${isAdult}`)
-    }
-isUserAdult(15)
-isUserAdult(25)
+    return age >= 18;
+}
+
+console.log(isUserAdult(25)); 
+console.log(isUserAdult(15)); 
