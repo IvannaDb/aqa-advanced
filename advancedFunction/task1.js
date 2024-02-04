36921de8bb9e84f35599e576c1147db0ec4f114d
IvannaDb <ivanna.guba@gmail.com>
2024-02-04 14:26:03 +0200

    Added folder with file with  task1

---
+++ advancedFunction/task1.js
@@ -0,0 +1,47 @@
+// FIRST VARIANT
+console.log ('Using try..catch')
+function handleNum (number, isEvenNumberCb, isOddNumberCb)
+{
+    try{
+        if(number % 2 === 0){
+        isEvenNumberCb(number)
+        } else {
+            throw new Error(`${number} is odd`)
+        }
+        }
+        catch (e){
+        isOddNumberCb(e)
+    }
+}
+
+function handleEven (number){
+    console.log(`${number} is even`)
+}
+
+function handleOdd(error){
+    console.log(error.message)
+}
+
+handleNum(5, handleEven, handleOdd)
+handleNum(8, handleEven, handleOdd)
+
+// SECOND VARIANT
+console.log ('Using if...else')
+function handleNum2(number2, isEvenNumberCb2, isOddNumberCb2) {
+    if (number2 % 2 === 0) {
+        isEvenNumberCb2(number2);
+    } else {
+        isOddNumberCb2(number2);
+    }
+}
+
+function handleEven2(number2) {
+    console.log(`${number2} is even`);
+}
+
+function handleOdd2(number2) {
+    console.log(`${number2} is odd`);
+}
+
+handleNum2(11, handleEven2, handleOdd2);
+handleNum2(20, handleEven2, handleOdd2);
