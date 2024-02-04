022431a5fba9321218775259441bb9762db702be
IvannaDb <ivanna.guba@gmail.com>
2024-02-04 12:30:12 +0200

    Added new folder with task1

---
+++ introToFunctions/task1.js
@@ -0,0 +1,6 @@
+function calculateAreaOfRectangle (width,height) {
+const areaOfRectangle = width*height
+return areaOfRectangle
+}
+const result = calculateAreaOfRectangle(5,10)
+console.log ('Result of calculating the area of rectangle is', result)
