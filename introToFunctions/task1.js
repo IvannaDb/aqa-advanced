1af3885af8913da8fe3537c318780907408b31e5
IvannaDb <ivanna.guba@gmail.com>
2024-02-04 16:13:19 +0200

    Added changes after review

--- introToFunctions/task1.js
+++ introToFunctions/task1.js
@@ -3,4 +3,17 @@ const areaOfRectangle = width*height
 return areaOfRectangle
 }
 const result = calculateAreaOfRectangle(5,10)
-console.log ('Result of calculating the area of rectangle is', result)
+console.log ('Result of calculating the area of rectangle with "Function declaration" is ', result)
+
+
+const areaOfRectangle2 = function (width,height) {
+    const areaOfRectangle2 = width*height
+    return areaOfRectangle2
+    }
+    console.log ('Result of calculating the area of rectangle with "Function expression" is', areaOfRectangle2(3,15))
+
+
+const areaOfRectangle3 = (width,height) => {
+    return width*height
+}
+console.log ('Result of calculating the area of rectangle with "Arrow function" is', areaOfRectangle3(2,30))
--- introToFunctions/task2.js
+++ introToFunctions/task2.js
@@ -1,7 +1,14 @@
-function isUserAdult(...ages) {
-    for (const age of ages) {
+// function isUserAdult(...ages) {
+//     for (const age of ages) {
+//         const isAdult=age>= 18
+//         console.log(`Is this person ${age} years old an adult? ${isAdult}`);
+//     }
+// }
+// isUserAdult(15, 25);
+
+function isUserAdult(age) {
         const isAdult=age>= 18
-        console.log(`Is this person ${age} years old an adult? ${isAdult}`);
+        console.log(`Is this person ${age} years old an adult? ${isAdult}`)
     }
-}
-isUserAdult(15, 25);
+isUserAdult(15)
+isUserAdult(25)
