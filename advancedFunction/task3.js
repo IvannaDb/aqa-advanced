5b98fbc992b763df81dee9c5ef663eaba3c7e3ff
IvannaDb <ivanna.guba@gmail.com>
2024-02-04 16:36:05 +0200

    Updated after review

--- advancedFunction/task3.js
+++ advancedFunction/task3.js
@@ -6,7 +6,7 @@ function divide (numerator, denominator) {
         if (typeof denominator !== 'number' || typeof numerator !== 'number') {
         throw new Error("Чисельник та знаменник повинні бути числами!")
         }
-        let resultOfDivision = numerator/denominator
+        const resultOfDivision = numerator/denominator
         return resultOfDivision
 
     } catch (error) { 
@@ -16,9 +16,33 @@ function divide (numerator, denominator) {
 }
 try {
     console.log(divide(35, 2))  
+} catch (error) {
+    console.log("Помилка виклику функції divide:", error.message)
+} finally {
+    console.log ('Робота завершена')
+}
+try { 
     console.log(divide(5, 0))  
+} catch (error) {
+    console.log("Помилка виклику функції divide:", error.message)
+} finally {
+    console.log ('Робота завершена')
+}
+try { 
     console.log(divide('a', 6))
+} catch (error) {
+    console.log("Помилка виклику функції divide:", error.message)
+} finally {
+    console.log ('Робота завершена')
+}
+try {
     console.log(divide(5, 'b'))
+} catch (error) {
+    console.log("Помилка виклику функції divide:", error.message)
+}finally {
+    console.log ('Робота завершена')
+}
+try {
     console.log(divide(5, 10))
 } catch (error) {
     console.log("Помилка виклику функції divide:", error.message)
